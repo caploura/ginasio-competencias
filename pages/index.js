8c4f250c2ccefb6a0625778fc4850276d753087b
Carlos Loura <carlos.loura@getir.com>
2022-09-30 17:04:06 +0200

    Created UnderConstruction page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import { CgPhone } from 'react-icons/cg';
+import { MdEmail } from 'react-icons/md';
+import { FaInstagram } from 'react-icons/fa';
+import { AiFillFacebook } from 'react-icons/ai';
+
+const logo_no_bg = '/logos/logo-blue-bg.png';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>O Website está em construção</h1>
+      <section className={styles.welcome}>
+        <img src={logo_no_bg} className={styles.mainLogo}/>
+        <h1>O Website está em construção</h1>
+      </section>
+
+      <section className={styles.contactInfo}>
+        <h3 className={styles.stringWithIcon}> <CgPhone/> +351 965 148 128</h3>
+        <h3 className={styles.stringWithIcon}> <MdEmail/> geral@ginasio-competencias.pt</h3>
+      </section>
+
+      <section className={styles.socialMedia}>
+        <a href="https://www.instagram.com/ginasio_de_competencias/" target="_blank">
+          <FaInstagram/>
+        </a>
+        <a href="https://www.facebook.com/ginasiodecompetencias/" target="_blank">
+          <AiFillFacebook />
+        </a>
+      </section>
     </div>
   )
 }
